4cf7e46063022792aab35e7fb1bb85ff4056fe4f
hirokiky <hirokiky@gmail.com>
2013-09-05 23:32:38 +0900

    Improved path_matching to handle nested matching currentry

--- gargant/dispatch/matching.py
+++ gargant/dispatch/matching.py
@@ -6,31 +6,31 @@ def method_matching(method, picker=lambda condition: condition):
     return _matching
 
 
-def brace_match(s):
-    if s.startswith('{') and s.endswith('}'):
-        return s.strip('{}')
+PATH_MATCHING_LIST_NAME = 'gargant.dispatch.path_matching_list'
 
 
 def path_matching(matching_list, picker=lambda condition: condition):
     def _matching(condition):
         environ = picker(condition)
-        url_kwargs = {'matching_list': matching_list}
-        path_info = environ.get('PATH_INFO', '')
-        path_list = path_info.split('/')[1:]
 
-        if len(path_list) != len(matching_list):
+        url_kwargs = {'matching_list': matching_list}
+        path_list = environ.get(PATH_MATCHING_LIST_NAME)
+        if not path_list:
+            path_info = environ.get('PATH_INFO', '')
+            path_list = path_info.split('/')
+        print path_list
+        if path_list < matching_list:
             return None
 
-        for path, matching in map(None, path_list, matching_list):
-            key = brace_match(matching)
-            if key:
+        for path, matching in zip(path_list, matching_list):
+            if matching.startswith('{') and matching.endswith('}'):
+                key = matching.strip('{}')
                 url_kwargs[key] = path
-                continue
             else:
                 if path != matching:
                     return None
-                else:
-                    continue
-        else:
-            return url_kwargs
+
+        environ[PATH_MATCHING_LIST_NAME] = path_list[len(matching_list):]
+
+        return url_kwargs
     return _matching
