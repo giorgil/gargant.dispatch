88a0188a265ee710bc8170841f9e3ab3a6e1086e
hirokiky <hirokiky@gmail.com>
2013-09-08 13:52:39 +0900

    Added tests:
    - First, Adding tests for matching functions

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='gargant.dispatch',
     version='0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     namespace_packages=['gargant'],
     url='https://github.com/hirokiky/gargant.dispatch',
     license='MIT',
     author='hirokiky',
     author_email='hirokiky@gmail.com',
-    description='Dispatcher for WSGI Applications'
+    description='Dispatcher for WSGI Applications',
+    tests_require=['pytest==2.3.5']
 )
---
+++ tests/test_matching.py
@@ -0,0 +1,64 @@
+import pytest
+from wsgiref.util import setup_testing_defaults
+
+
+def get_dummy_environ(base_environ={}):
+    environ = base_environ
+    setup_testing_defaults(environ)
+    return environ
+
+
+class TestMethodMatching(object):
+    @pytest.fixture
+    def target(self):
+        from gargant.dispatch.matching import method_matching
+        return method_matching
+
+    def test_matched(self, target):
+        environ = get_dummy_environ()
+        assert target('get')(environ)
+
+    def test_not_matched(self, target):
+        environ = get_dummy_environ()
+        assert not target('post')(environ)
+
+
+def get_path_list(environ):
+    from gargant.dispatch.matching import PATH_MATCHING_LIST_NAME
+    return environ.get(PATH_MATCHING_LIST_NAME)
+
+
+class TestPathMatching(object):
+    @pytest.fixture
+    def target(self):
+        from gargant.dispatch.matching import path_matching
+        return path_matching
+
+    def test_matched(self, target):
+        environ = get_dummy_environ()
+        assert target(['', ''])(environ)
+        assert get_path_list(environ) == []
+
+    def test_matched_child(self, target):
+        environ = get_dummy_environ({'PATH_INFO': '/members/ritsu'})
+        assert target(['', 'members'])(environ)
+        assert get_path_list(environ) == ['ritsu']
+
+    def test_not_matched(self, target):
+        environ = get_dummy_environ({'PATH_INFO': '/members'})
+        assert not target(['', 'instruments'])(environ)
+
+    def test_with_path_list(self, target):
+        from gargant.dispatch.matching import PATH_MATCHING_LIST_NAME
+        environ = get_dummy_environ({PATH_MATCHING_LIST_NAME: ['members', 'ritsu']})
+        assert target(['members', 'ritsu'])(environ)
+
+    def test_path_list_too_short(self, target):
+        environ = get_dummy_environ({'PATH_INFO': '/members'})
+        assert not target(['', 'members', 'ritsu'])(environ)
+
+    def test_with_matching_pattern(self, target):
+        environ = get_dummy_environ({'PATH_INFO': '/members/ritsu'})
+        actual = target(['', 'members', '{member}'])(environ)
+        assert actual == {'member': 'ritsu',
+                          'matching_list': ['', 'members', '{member}']}
