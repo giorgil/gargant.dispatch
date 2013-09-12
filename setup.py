fb678d08cc2ad96696dff10e8a87ed4ba05afe12
hirokiky <hirokiky@gmail.com>
2013-09-13 00:55:32 +0900

    Improved Readme.rst to upload to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
 
 setup(
     name='gargant.dispatch',
@@ -9,7 +13,8 @@ setup(
     license='MIT',
     author='hirokiky',
     author_email='hirokiky@gmail.com',
-    description='Dispatcher for WSGI Applications',
+    description='Flexible dispatcher for WSGI Applications',
+    long_description=README,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
